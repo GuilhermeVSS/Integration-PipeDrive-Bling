16b1114667fbe402a0b5d0f180593ee86cdbee67
Guilherme Ventura <gs@inicie.digital>
2022-01-09 15:29:22 -0300

    feat(connection): add a connection to mongo

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const routes = require('./routes.js');
+const database = require('./database');
 
 class App {
     constructor(){
         this.server = express();
         this.middleware();
         this.routes();
+        this.connection();
     }
     middleware(){
         this.server.use(express.json());
     }
     routes(){
         this.server.use(routes);
+    }
+
+    connection(){
+        this.mongo = database.init();
     }
 }
 
